d4c5cd7c2e07f069c1741093821af2cb0c47d1e0
rsy1 <royal.siyanata@gmail.com>
2022-09-11 22:36:53 +0200

    daily comit

---
+++ rsy1-moblie-app-ws/src/test/java/com/apsdeveloper/appws/Rsy1MoblieAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apsdeveloper.appws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rsy1MoblieAppWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
